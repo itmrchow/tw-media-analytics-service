1e27fa9cbaa6dcbc64d14ace75d19948940069cb
Jeff Wu <itmrchow@gmail.com>
2025-06-09 11:45:00 +0800

    feat: enhance logging and tracing in InitCron function
    - Updated tracing span name for better context identification
    - Improved error logging to include job names for clearer diagnostics
    - Enhanced integration with OpenTelemetry for cron job functions

--- infra/cronjob.go
+++ infra/cronjob.go
@@ -12,8 +12,7 @@ import (
 // InitCron 初始化 cron job.
 func InitCron(ctx context.Context, logger *zerolog.Logger, jobs *cronjob.CronJob) {
 	// Trace
-	tracer := getInfraTracer()
-	ctx, span := tracer.Start(ctx, "InitCron")
+	ctx, span := tracer.Start(ctx, "infra/cronjob:InitCron")
 	logger.Info().Ctx(ctx).Msg("InitCron: start")
 	defer func() {
 		span.End()
@@ -24,13 +23,13 @@ func InitCron(ctx context.Context, logger *zerolog.Logger, jobs *cronjob.CronJob
 	// ArticleScrapingJob
 	_, err := c.AddFunc("0 * * * *", jobs.ArticleScrapingJob)
 	if err != nil {
-		logger.Fatal().Err(err).Ctx(ctx).Msg("failed to add cron job")
+		logger.Fatal().Err(err).Ctx(ctx).Str("job", "ArticleScrapingJob").Msg("failed to add cron job")
 	}
 
 	// AnalyzeNewsJob
 	_, err = c.AddFunc("*/1 * * * *", jobs.AnalyzeNewsJob)
 	if err != nil {
-		logger.Fatal().Err(err).Ctx(ctx).Msg("failed to add cron job")
+		logger.Fatal().Err(err).Ctx(ctx).Str("job", "AnalyzeNewsJob").Msg("failed to add cron job")
 	}
 
 	c.Start()
