bb1b801f1fc7d82ac31df4457ff17d19ebdafdfc
Jeff Wu <itmrchow@gmail.com>
2025-06-18 18:01:10 +0800

    feat: add AI model initialization function
    - Introduced InitAIModel function for initializing the AI model
    - Integrated OpenTelemetry tracing for monitoring the initialization process
    - Added logging for start and end of the AI model initialization
    - Handled errors during the creation of the Gemini model with appropriate logging

--- domain/utils/ai/aimodel.go
+++ domain/utils/ai/aimodel.go
@@ -1,14 +1,15 @@
-package infra
+package ai
 
 import (
 	"context"
 
 	"github.com/rs/zerolog"
+	"go.opentelemetry.io/otel/trace"
 
 	"itmrchow/tw-media-analytics-service/domain/ai"
 )
 
-func InitAIModel(ctx context.Context, initLogger *zerolog.Logger) ai.AiModel {
+func InitAIModel(ctx context.Context, logger *zerolog.Logger, tracer trace.Tracer) ai.AiModel {
 	// Trace
 	ctx, span := tracer.Start(ctx, "infra/InitAIModel: Init AI Model")
 	logger.Info().Ctx(ctx).Msg("InitAIModel: start")
@@ -18,7 +19,7 @@ func InitAIModel(ctx context.Context, initLogger *zerolog.Logger) ai.AiModel {
 	}()
 
 	// New Gemini
-	model, err := ai.NewGemini(ctx, initLogger)
+	model, err := ai.NewGemini(ctx, logger)
 	if err != nil {
 		logger.Fatal().Err(err).Ctx(ctx).Msg("InitAIModel: failed to create Gemini model")
 	}
